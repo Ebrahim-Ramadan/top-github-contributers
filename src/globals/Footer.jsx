0195ccf776d9939ff8a87c68f4d95ec57c2931fa
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2024-07-03 16:22:54 +0200

    fixed the graident bg on footer

--- src/globals/Footer.jsx
+++ src/globals/Footer.jsx
@@ -3,7 +3,7 @@ import yapping from '/yapping.svg'
 
 export const Footer = () => {
   return (
-      <div className='bg-gradient-to-b from-black to-transparent h-14 bottom-6 md:bottom-0 z-40 text-sm  p-4 w-full fixed justify-between flex md:flex-row gap-2 px-4 flex-col'>
+      <div className='bg-gradient-to-t from-black to-transparent h-14 bottom-6 md:bottom-0 z-40 text-sm  p-4 w-full fixed justify-between flex md:flex-row gap-2 px-4 flex-col'>
       <div className='flex items-center flex-row'>
       created by
           <a href='https://x.com/scoopsahoykid' className='px-1' target='_blank'>
